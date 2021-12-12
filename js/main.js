f92679b053c36864f80c8ee889d6e18d6a4571dc
yukasan <yuka11on7@gmail.com>
2021-12-13 08:47:13 +0900

    main

--- js/main.js
+++ js/main.js
@@ -14,6 +14,19 @@ console.log(say);
 //条件分岐　問題１
 const price = 1000;
 const tax = 0.1;
-console.log(price * tax)
+console.log(price * tax);
 
 //条件分岐　問題２
+const scoreA = 80 + 90 + 60 + 85 + 100;
+const scoreB = 40 + 30 + 50 + 25 + 60;
+
+console.log(scoreA);
+console.log(scoreB);
+
+if (scoreA >= 300 && scoreB >= 300) {
+  console.log("素晴らしい");
+} else if (scoreA >= 300 || scoreB >= 300) {
+  console.log("普通");
+} else if (scoreA < 300 && scoreB < 300) {
+  console.log("頑張ろう");
+}
