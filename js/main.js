b29d5976e7ad9aa4ca240b0627b7bac6ed02be92
yukasan <yuka11on7@gmail.com>
2021-12-13 08:10:42 +0900

    main

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//コメントです
+console.log("Hello world");//コメントです
+
+let message = "Hello world"; //「message」という変数を定義し、「Hello world」という値を代入
+console.log(message); //変数名を使うことで、変数の中の値を使うことができる
+message = "Good night world"; //変数の値は書き換えることができる
+console.log(message); //中身が変わるので、出力される値も変わる
+
+let say = "Hello";
+console.log(say);
+say = "Goodbye";
+console.log(say);
+
+//条件分岐　問題１
+const price = 1000;
+const tax = 0.1;
+console.log(price * tax)
+
+//条件分岐　問題２
